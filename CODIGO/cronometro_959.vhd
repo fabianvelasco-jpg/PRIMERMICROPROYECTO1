f6ecb4421f665b0216ed0f155ee247f4e756b4d0
fabianvelasco-jpg <fabianvelasco@unicauca.edu.co>
2026-09-21 07:53:14 -0500

    Cambio en la lista de sensibilidad
    Agregué los botones de inicio y pausa a la lista de sensibilidad del process, a demás de quitar la condicional dentro del flanco de subida y dejarla fuera, pero aun dentro del process.

--- CODIGO/cronometro_959.vhd
+++ CODIGO/cronometro_959.vhd
@@ -20,23 +20,23 @@ architecture logica of cronometro_959 is
     signal cuentaMin    : unsigned(3 downto 0);
     signal estadoActivo : std_logic;
 begin
-    process (relojBase, botonReinicio)
+    process (relojBase, botonReinicio, botonArranque, botonParada)
     begin
         if botonReinicio = '1' then
             cuentaUniSec <= (others => '0');
             cuentaDecSec <= (others => '0');
             cuentaMin    <= (others => '0');
             estadoActivo <= '0';
+				
+        elsif botonArranque = '1' then
+            estadoActivo <= '1'; 
             
+        elsif botonParada = '1' then
+            estadoActivo <= '0';   
+				
+				
         elsif relojBase'event and relojBase = '1' then
-            
-            -- Control de los botones de acción
-            if botonArranque = '1' then
-                estadoActivo <= '1';
-            elsif botonParada = '1' then
-                estadoActivo <= '0';
-            end if;
-            
+               
             -- Lógica de conteo del reloj
             if estadoActivo = '1' then
                 -- Si llega a 9:59, se detiene automáticamente
